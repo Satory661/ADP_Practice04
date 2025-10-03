1b45cf31f4a376ebc31e607f1d529631313791cb
Satory661 <gafurovelizar@gmail.com>
2025-10-03 23:24:39 +0500

    END

---
+++ ADP_Practice04 /ADP_Practice04/ADP_Practice04/Creators/DocumentCreator.cs
@@ -0,0 +1,5 @@
+namespace ADP_Practice04
+public abstract class DocumentCreator
+{
+    public abstract Document CreateDocument();
+}
---
+++ ADP_Practice04 /ADP_Practice04/ADP_Practice04/Work/Letter.cs
@@ -0,0 +1,9 @@
+namespace ADP_Practice04;
+
+public class Letter: Document
+{
+    public override void Open()
+    {
+        Console.WriteLine("Letter opened");
+    }
+}
